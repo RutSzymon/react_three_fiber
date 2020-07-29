3028e6b665db16930444134d2e970b983c1de26e
RutSzymon <rut.szymon@gmail.com>
2020-07-29 13:03:50 +0200

    Added index.js and styles.css

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Canvas } from "react-three-fiber";
+import "./styles.css";
+
+function App() {
+  return <Canvas />;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
