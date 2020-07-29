cb0e0b7ca83c2ad6e9e26552557e166356cbe0e6
RutSzymon <rut.szymon@gmail.com>
2020-07-29 14:50:09 +0200

    Added more lights

--- src/index.js
+++ src/index.js
@@ -24,9 +24,8 @@ function Light({ brightness, color }) {
 function GroundPlane() {
   return (
     <mesh receiveShadow rotation={[5, 0, 0]} position={[0, -1, 0]}>
-      {" "}
-      <planeBufferGeometry attach="geometry" args={[500, 500]} />{" "}
-      <meshStandardMaterial attach="material" color="white" />{" "}
+      <planeBufferGeometry attach="geometry" args={[500, 500]} />
+      <meshStandardMaterial attach="material" color="white" />
     </mesh>
   );
 }
@@ -34,9 +33,8 @@ function GroundPlane() {
 function BackDrop() {
   return (
     <mesh receiveShadow position={[0, -1, -5]}>
-      {" "}
-      <planeBufferGeometry attach="geometry" args={[500, 500]} />{" "}
-      <meshStandardMaterial attach="material" color="white" />{" "}
+      <planeBufferGeometry attach="geometry" args={[500, 500]} />
+      <meshStandardMaterial attach="material" color="white" />
     </mesh>
   );
 }
@@ -44,26 +42,73 @@ function BackDrop() {
 function Sphere() {
   return (
     <mesh visible userData={{ test: "hello" }} position={[0, 0, 0]} castShadow>
-      {" "}
-      <sphereGeometry attach="geometry" args={[1, 16, 16]} />{" "}
+      <sphereGeometry attach="geometry" args={[1, 16, 16]} />
       <meshStandardMaterial
         attach="material"
         color="white"
         transparent
         roughness={0.1}
         metalness={0.1}
-      />{" "}
+      />
     </mesh>
+  );
+}
+
+// Lights
+
+function KeyLight({ brightness, color }) {
+  return (
+    <rectAreaLight
+      width={3}
+      height={3}
+      color={color}
+      intensity={brightness}
+      position={[-2, 0, 5]}
+      lookAt={[0, 0, 0]}
+      penumbra={1}
+      castShadow
+    />
+  );
+}
+
+function FillLight({ brightness, color }) {
+  return (
+    <rectAreaLight
+      width={3}
+      height={3}
+      intensity={brightness}
+      color={color}
+      position={[2, 1, 4]}
+      lookAt={[0, 0, 0]}
+      penumbra={2}
+      castShadow
+    />
+  );
+}
+
+function RimLight({ brightness, color }) {
+  return (
+    <rectAreaLight
+      width={2}
+      height={2}
+      intensity={brightness}
+      color={color}
+      position={[1, 4, -2]}
+      rotation={[0, 180, 0]}
+      castShadow
+    />
   );
 }
 
 function App() {
   return (
     <Canvas>
-      <Light brightness={10} color={"white"} />
-      <Sphere />
       <BackDrop />
       <GroundPlane />
+      <KeyLight brightness={5.6} color="#ffbdf4" />
+      <FillLight brightness={2.6} color="#bdefff" />
+      <RimLight brightness={54} color="#fff" />
+      <Sphere />
     </Canvas>
   );
 }
