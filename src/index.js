1d38056741b3a9505ce61800423b1239de39f3e9
RutSzymon <rut.szymon@gmail.com>
2020-07-29 13:43:41 +0200

    Added sphere

--- src/index.js
+++ src/index.js
@@ -3,8 +3,28 @@ import ReactDOM from "react-dom";
 import { Canvas } from "react-three-fiber";
 import "./styles.css";
 
+function Sphere() {
+  return (
+    <mesh visible userData={{ test: "hello" }} position={[0, 0, 0]} castShadow>
+      {" "}
+      <sphereGeometry attach="geometry" args={[1, 16, 16]} />{" "}
+      <meshStandardMaterial
+        attach="material"
+        color="white"
+        transparent
+        roughness={0.1}
+        metalness={0.1}
+      />{" "}
+    </mesh>
+  );
+}
+
 function App() {
-  return <Canvas />;
+  return (
+    <Canvas>
+      <Sphere />
+    </Canvas>
+  );
 }
 
 const rootElement = document.getElementById("root");
