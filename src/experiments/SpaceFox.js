7da0cbf69c6f12bb8a09766b6c14be5ebb92cb3a
RutSzymon <rut.szymon@gmail.com>
2020-07-30 15:18:40 +0200

    Added ship movements

--- src/experiments/SpaceFox.js
+++ src/experiments/SpaceFox.js
@@ -1,4 +1,4 @@
-import React, { Suspense, useRef } from "react";
+import React, { Suspense, useRef, useState } from "react";
 import {
   Canvas,
   useLoader,
@@ -59,17 +59,34 @@ function Terrain() {
 }
 
 function ArWing() {
-  const group = useRef();
+  const [shipPosition, setShipPosition] = useState();
+
+  const ship = useRef();
+  useFrame(({ mouse }) => {
+    setShipPosition({
+      position: { x: mouse.x * 6, y: mouse.y * 2 },
+      rotation: { z: -mouse.x * 0.5, x: -mouse.x * 0.5, y: -mouse.y * 0.2 },
+    });
+  });
+  // Update the ships position from the updated state.
+  useFrame(() => {
+    ship.current.rotation.z = shipPosition.rotation.z;
+    ship.current.rotation.y = shipPosition.rotation.x;
+    ship.current.rotation.x = shipPosition.rotation.y;
+    ship.current.position.y = shipPosition.position.y;
+    ship.current.position.x = shipPosition.position.x;
+  });
+
   const { nodes } = useLoader(GLTFLoader, "3d_models/SpaceFox/arwing.glb");
 
   return (
-    <group ref={group}>
+    <group ref={ship}>
       <mesh visible geometry={nodes.Default.geometry}>
         <meshStandardMaterial
           attach="material"
           color="white"
-          roughness={0.3}
-          metalness={0.3}
+          roughness={1}
+          metalness={0}
         />
       </mesh>
     </group>
