77ff74194b120953dd65a3df73bd9717da943ded
RutSzymon <rut.szymon@gmail.com>
2020-07-30 14:30:05 +0200

    Added SpaceFox experiment scaffold

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ThreePointLighting from "./experiments/ThreePointLighting";
 import SkyboxApp from "./experiments/Skybox";
+import SpaceFox from "./experiments/SpaceFox";
 
 function App() {
   // return <ThreePointLighting />;
-  return <SkyboxApp />;
+  // return <Skybox />;
+  return <SpaceFox />;
 }
 
 const rootElement = document.getElementById("root");
