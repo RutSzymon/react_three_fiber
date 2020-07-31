249f093a8155e24c7d4266ecf15b8b0c75402a42
RutSzymon <rut.szymon@gmail.com>
2020-07-31 10:56:32 +0200

    Added game setting constants

--- src/experiments/SpaceFox.js
+++ src/experiments/SpaceFox.js
@@ -22,6 +22,10 @@ import "./styles.css";
 extend({ OrbitControls });
 
 const GROUND_HEIGHT = -50; // A Constant to store the ground height of the game.
+const LASER_RANGE = 100;
+const LASER_Z_VELOCITY = 1;
+const ENEMY_SPEED = 0.1;
+const GROUND_HEIGHT = -50;
 
 function Loading() {
   return (
