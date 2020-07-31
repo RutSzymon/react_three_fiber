666348d489563b5720734727758e1e4f29611bff
RutSzymon <rut.szymon@gmail.com>
2020-07-31 10:25:37 +0200

    Added shipPositionState atom

--- src/components/gameState.js
+++ src/components/gameState.js
@@ -1,1 +1,6 @@
 import { atom } from "recoil";
+
+export const shipPositionState = atom({
+  key: "shipPosition", // unique ID (with respect to other atoms/selectors)
+  default: { position: {}, rotation: {} }, // default value (aka initial value)
+});
