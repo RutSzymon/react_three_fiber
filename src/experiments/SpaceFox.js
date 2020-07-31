bcad577b8bd0b8529b50c6d0a7da4fb682aa12e2
RutSzymon <rut.szymon@gmail.com>
2020-07-31 11:01:35 +0200

    Added GameTimer component

--- src/experiments/SpaceFox.js
+++ src/experiments/SpaceFox.js
@@ -14,6 +14,7 @@ import {
   shipPositionState,
   laserPositionState,
   enemyPositionState,
+  scoreState,
 } from "../components/gameState";
 
 import "./styles.css";
@@ -21,7 +22,6 @@ import "./styles.css";
 // Extend will make OrbitControls available as a JSX element called orbitControls for us to use.
 extend({ OrbitControls });
 
-const GROUND_HEIGHT = -50; // A Constant to store the ground height of the game.
 const LASER_RANGE = 100;
 const LASER_Z_VELOCITY = 1;
 const ENEMY_SPEED = 0.1;
@@ -204,6 +204,54 @@ function Lasers() {
   );
 }
 
+// This component runs game logic on each frame draw to update game state.
+function GameTimer() {
+  const [enemies, setEnemies] = useRecoilState(enemyPositionState);
+  const [lasers, setLaserPositions] = useRecoilState(laserPositionState);
+  const [score, setScore] = useRecoilState(scoreState);
+
+  useFrame(({ mouse }) => {
+    // Map through all of the enemies in state. Detect if each enemy is within one unit of a laser if they are set that place in the return array to true.
+    // The result will be an array where each index is either a hit enemy or an unhit enemy.
+    const hitEnemies = enemies
+      ? enemies.map(
+          (enemy) =>
+            lasers.filter(
+              (laser) =>
+                laser.z - enemy.z < 1 &&
+                laser.x - enemy.x < 1 &&
+                laser.y - enemy.y < 1
+            ).length > 0
+        )
+      : [];
+
+    if (hitEnemies.includes(true) && enemies.length > 0) {
+      setScore(score + hitEnemies.filter((hit) => hit).length);
+      console.log("hit detected");
+    }
+
+    // Move all of the enemies. Remove enemies that have been destroyed, or that have passed the player.
+    setEnemies(
+      enemies
+        .map((enemy) => ({ x: enemy.x, y: enemy.y, z: enemy.z + ENEMY_SPEED }))
+        .filter((enemy, idx) => !hitEnemies[idx] && enemy.z < 0)
+    );
+    // Move the Lasers and remove lasers at end of range or that have hit the ground.
+    setLaserPositions(
+      lasers
+        .map((laser) => ({
+          id: laser.id,
+          x: laser.x + laser.velocity[0],
+          y: laser.y + laser.velocity[1],
+          z: laser.z - LASER_Z_VELOCITY,
+          velocity: laser.velocity,
+        }))
+        .filter((laser) => laser.z > -LASER_RANGE && laser.y > GROUND_HEIGHT)
+    );
+  });
+  return null;
+}
+
 const CameraControls = () => {
   // Get a reference to the Three.js Camera, and the canvas html element.
   // We need these to setup the OrbitControls component.
@@ -242,6 +290,7 @@ export default function App() {
         <Lasers />
         <Terrain />
         <LaserController />
+        <GameTimer />
       </RecoilRoot>
     </Canvas>
   );
