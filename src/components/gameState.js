85480c21f9910c5d74bd37a4abc1bacff524a80e
RutSzymon <rut.szymon@gmail.com>
2020-07-31 10:26:43 +0200

    Added enemyPositionState atom

--- src/components/gameState.js
+++ src/components/gameState.js
@@ -4,3 +4,11 @@ export const shipPositionState = atom({
   key: "shipPosition", // unique ID (with respect to other atoms/selectors)
   default: { position: {}, rotation: {} }, // default value (aka initial value)
 });
+
+export const enemyPositionState = atom({
+  key: "enemyPosition", // unique ID (with respect to other atoms/selectors)
+  default: [
+    { x: -10, y: 10, z: -80 },
+    { x: 20, y: 0, z: -100 },
+  ], // default value (aka initial value)
+});
